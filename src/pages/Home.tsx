212b87d95c55a3be1e79fb242b0e7424681ef197
unknown <jagannatha.ks@flynavaweb.in>
2023-04-30 07:50:54 +0530

    resolved comments

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,6 +1,6 @@
 import { FC, useState } from "react";
 import JobCategoryPannel from "../components/JobCategoryPannel";
-import CustomModal from "../components/custom/CustomModal/CustomModal";
+import CustomModal from "../components/custom/CustomModal";
 import JobsModal from "../components/modals/JobsModal";
 import CustomButton from "../components/custom/CustomButton";
 import { resources } from "../util/resources";
@@ -8,10 +8,7 @@ import { resources } from "../util/resources";
 const Home: FC = () => {
   const [open, setOpen] = useState<boolean>(false);
 
-  const handleClose = () => {
-    setOpen(false);
-  }
-  const handleOpenOpup = () => {
+  const handlepopup = () => {
     setOpen(!open);
   }
   return (
@@ -23,13 +20,13 @@ const Home: FC = () => {
           <JobsModal />
         }
         width={500}
-        okCancel={handleClose}
+        okCancel={handlepopup}
       />
       <div className='flex justify_end mt_16'>
         <CustomButton
           label={resources?.postJob}
           className='white bg_blue row_center w_max py br_8_0_8_8 h_max fs_16 fw_600 lh_16'
-          onClick={handleOpenOpup}
+          onClick={handlepopup}
         />
       </div>
       <JobCategoryPannel />
